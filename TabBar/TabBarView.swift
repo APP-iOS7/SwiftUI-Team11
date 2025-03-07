96e1f1a58caae293c9fa587868b18009eec126bd
Yeonah Shim <simya0528@naver.com>
2025-03-07 15:35:24 +0900

    탭바 뷰 완료

---
+++ TabBar/TabBarView.swift
@@ -0,0 +1,59 @@
+//
+//  TabBarView.swift
+//  MovieDiary
+//
+//  Created by 심연아 on 3/7/25.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    @State private var selectedTab = 0
+    @State private var showCreateThreadView = false
+    
+    var body: some View {
+        TabView(selection: $selectedTab) {
+            ContentView()
+                .tabItem {
+                    Image(systemName: selectedTab == 0 ? "house.fill" : "house")
+                        .environment(\.symbolVariants, selectedTab == 0 ? .fill : .none)
+                    Text("홈")
+                }
+                .onAppear { selectedTab = 0 }
+                .tag(0)
+            Text("")
+                .tabItem {
+                    Image(systemName: "plus")
+                        .environment(\.symbolVariants, selectedTab == 0 ? .fill : .none)
+                    Text("기록")
+                }
+                .onAppear { selectedTab = 2 }
+                .tag(2)
+            
+            ContentView()
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                        .environment(\.symbolVariants, selectedTab == 0 ? .fill : .none)
+                    Text("검색")
+                }
+                .onAppear { selectedTab = 1 }
+                .tag(1)
+
+        }
+        .onChange(of: selectedTab) { oldValue, newValue in
+            showCreateThreadView = newValue == 2
+        }
+
+        .sheet(isPresented: $showCreateThreadView, onDismiss: {
+            selectedTab = 0
+        }, content: {
+            ContentView()
+            //여길 이제 CommentView로 바꿔주면 됩니다.
+        })
+        .tint(.black)
+    }
+}
+
+#Preview {
+    TabBarView()
+}
