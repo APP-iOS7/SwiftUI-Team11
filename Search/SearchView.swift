e9fd1b8c4993a292fd2595b062a68676d7162f0c
Yeonah Shim <simya0528@naver.com>
2025-03-07 11:17:44 +0900

    검색 화면 구상중

--- Search/SearchView.swift
+++ Search/SearchView.swift
@@ -8,8 +8,23 @@
 import SwiftUI
 
 struct SearchView: View {
+    @State private var searchText = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView {
+                LazyVStack() {
+                    ForEach(0 ... 10, id: \.self) { user in
+                        VStack {
+//                            UserCell()
+                            Divider()
+                        }
+                        .padding(.vertical, 6)
+                    }
+                }
+            }
+            .searchable(text: $searchText, prompt: "콘텐츠, 인물, 컬렉션, 유저 검색")
+        }
     }
 }
 
