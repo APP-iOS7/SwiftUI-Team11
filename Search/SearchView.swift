a9b04a8902ea3a00605f0438dc096c8d407f09c6
Yeonah Shim <simya0528@naver.com>
2025-03-07 10:19:52 +0900

    검색 페이지 만들었습니다

---
+++ Search/SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  MovieDiary
+//
+//  Created by 심연아 on 3/7/25.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchView()
+}
